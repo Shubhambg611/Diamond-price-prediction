0d9e4ecde43aea41ee49a0bc4982624c058603e6
Shubhambg611 <s.bgadhave611@gmail.com>
2024-01-08 17:02:24 +0530

    keep

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="DimondPricePrediction",
+    version='0.0.1',
+    author='shubham gadhave',
+    author_email='s.bgadhave611@gmail.com',
+    packages=find_packages()
+
+)
+
