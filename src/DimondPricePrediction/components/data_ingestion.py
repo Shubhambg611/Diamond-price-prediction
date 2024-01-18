fcaf60efe2e6402e78b670038ff8c8f901277efb
Shubhambg611 <s.bgadhave611@gmail.com>
2024-01-18 17:29:17 +0530

    data

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -6,12 +6,21 @@ from src.DimondPricePrediction.exception import customexception
 from dataclasses import dataclass
 from pathlib import path 
 
-class DataInjection:
+class DataIngestionConfig:
+    raw_data_path: str=os.path.join("artifact","raw.csv")
+    train_data_path: str=os.path.join("artifacts","train.csv")
+    test_data_path: str=os.path.join("artifacts","test.csv")
+
+class DataIngestion:
     def __init__(self):
-        pass
+        self.injection_config=DataIngestionConfig()
     
     def initiate_data_injection(self):
         logging.info("Data injection started")
+        
+        
+        os.makedirs(os.path.join(self.ingestion_config.raw_data_path),exists=True)
+        data.to_csv(self.ingestion_config.raw_data_path,index=False)
         
         try:
             data = pd.read_csv(Path(os.path.join("notebooks/data","train.csv")))
@@ -23,4 +32,3 @@ class DataInjection:
             train_data,test_data = train_test_split(data,test_size=0.25) 
             logging,info("train test split completed")
             
-               
