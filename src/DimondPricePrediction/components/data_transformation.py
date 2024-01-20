4a7b9dd542c5be4437381041e79e267c45fb6e08
Shubhambg611 <s.bgadhave611@gmail.com>
2024-01-20 17:40:46 +0530

    data transformation

--- src/DimondPricePrediction/components/data_transformation.py
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -26,20 +26,80 @@ class DataTransformation:
 
     def get_data_transformation(self):
         try:
-            pass
-        
+            logging.info("data transformation initiated")
+            
+            categorical_col = ['cut','color','clarity']
+            numerical_cols = ['carat','depth','table','x','y','z']
+            
+            # Define the custom ranking for each ordinal variable
+            cut_categories = ['Fair', 'Good', 'Very Good','Premium','Ideal']
+            color_categories = ['D', 'E', 'F', 'G', 'H', 'I', 'J']
+            clarity_categories = ['I1','SI2','SI1','VS2','VS1','VVS2','VVS1','IF']
+            
+            logging.info("Pipeline initiated")
+            ## numeric pipeline
+            num_pipeline=Pipeline(
+                steps=[
+                    ('imputer',SimpleImputer(strategy='median')),
+                    ('scaler',StandardScaler())
+                    
+                ]
+                
+            )
+            cat_pipeline=Pipeline(
+    
+                steps=[
+                    ('imputer',SimpleImputer(strategy='most_frequent')),
+                    ('ordinalencoder',OrdinalEncoder(categories=[cut_categories,color_categories,clarity_categories]))
+                    ('Scaler',StandaredScaler())
+                ]
+                
+            )
+            
+            preprocessor=ColumnTransformer([
+            ('num_pipeline',num_pipeline,numerical_cols),
+            ('cat_pipeline',cat_pipeline,categorical_col)    
+            ])
+            
+            return preprocessor
+            
+            
         except Exception as e:
             logging.info("Exception occured in the inintiate_dataTransformation")
             
-            
+            raise customexception(e,sys) 
             
             
     def initialize_data_transformation(self,train_path,test_path):
             
         try:
-                pass   
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
             
             
+            logging.info("reading of training and testing data")
+            logging.info(f"train dataframe head : \n{train_df.head().to_string()}")
+            logging.info(f"test dataframe head : \n{test_df.head().to_string()}")
+            
+            preprocessing_obj = self.get_data_transformation()
+            target_column_name = 'price'
+            drop_columns=[target_column_name,'id']
+            
+            input_feature_train_df = train_df.drop(columns = drop_columns,axis=1)
+            target_feature_train_df = train_df[target_column_name]
+            
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.fit_transform(input_feature_test_df)            
+
+            logging.info("applying pre processing on data")
+            
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+            
+        
+        
         except Exception as e:
             logging.info("Exception occured in the inintiate_dataTransformation")
             raise customexception(e,sys)
