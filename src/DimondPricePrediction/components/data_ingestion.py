f872fffbf9f8c3e3bc7363741a2340ff98b30c11
Shubhambg611 <s.bgadhave611@gmail.com>
2024-01-20 10:28:18 +0530

    data

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -30,10 +30,13 @@ class DataIngestion:
             logging.info("I have now performed train test split")
             
             train_data,test_data = train_test_split(data,test_size=0.25) 
-            logging,info("train test split completed")  
+            logging.info("train test split completed")  
+            train_data.to_csv(self.ingestion_config.train_data_path,index=False)
+            test_data.to_csv(self.ingestion_config.test_data_path,index=false)
             
+            logging.info("Data ingesion part is completed")
             
             
         except Expetion as e:
-            pass
+            logging.info("excpetion during occured ")
         
---
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -0,0 +1,8 @@
+from src.DimondPricePrediction.components.data_ingestion import DataIngestion
+
+
+import os
+import sys
+from src.DimondPricePrediction.logger import logging
+from src.DimondPricePrediction.exception import customexception
+import pandas as pd 
